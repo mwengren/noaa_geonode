102d71e264eb87f559ac5638521239dd1befadd6
Micah Wengren <micah.wengren@gmail.com>
2014-07-08 15:56:18 -0400

    Updated urls.py in line with GeoNode/master

--- noaa_geonode/urls.py
+++ noaa_geonode/urls.py
@@ -7,7 +7,12 @@ from django.views.generic import TemplateView
 
 import geonode.proxy.urls
 
+from geonode.api.urls import api
+
+import autocomplete_light
+
 # Setup Django Admin
+autocomplete_light.autodiscover()
 from django.contrib import admin
 admin.autodiscover()
 
@@ -24,11 +29,11 @@ sitemaps = {
 urlpatterns = patterns('',
 
     # Static pages
-    url(r'^$', 'geonode.views.index', {'template': 'site_index.html'}, name='home'),
+    url(r'^/?$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^help/$', TemplateView.as_view(template_name='help.html'), name='help'),
     url(r'^developer/$', TemplateView.as_view(template_name='developer.html'), name='developer'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
-    
+
     # Layer views
     (r'^layers/', include('geonode.layers.urls')),
 
@@ -39,10 +44,7 @@ urlpatterns = patterns('',
     (r'^catalogue/', include('geonode.catalogue.urls')),
 
     # Search views
-    (r'^search/', include('geonode.search.urls')),
-
-    # Upload views
-    (r'^upload/', include('geonode.upload.urls')),
+    url(r'^search/$', TemplateView.as_view(template_name='search/search.html'), name='search'),
 
     # Social views
     (r"^account/", include("account.urls")),
@@ -60,6 +62,8 @@ urlpatterns = patterns('',
                                        name='account_ajax_login'),
     url(r'^account/ajax_lookup$', 'geonode.views.ajax_lookup',
                                        name='account_ajax_lookup'),
+    url(r'^security/permissions/(?P<resource_id>\d+)$', 'geonode.security.views.resource_permissions',
+                                       name='resource_permissions'),
 
     # Meta
     url(r'^lang\.js$', TemplateView.as_view(template_name='lang.js', content_type='text/javascript'), name='lang'),
@@ -68,18 +72,37 @@ urlpatterns = patterns('',
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
                                   {'sitemaps': sitemaps}, name='sitemap'),
     (r'^i18n/', include('django.conf.urls.i18n')),
+    (r'^autocomplete/', include('autocomplete_light.urls')),
     (r'^admin/', include(admin.site.urls)),
-
+    (r'^groups/', include('geonode.groups.urls')),
+    (r'^documents/', include('geonode.documents.urls')),
+    (r'^services/', include('geonode.services.urls')),
+    url(r'', include(api.urls)),
     )
 
-#Documents views
-if settings.DOCUMENTS_APP:
+if "geonode.contrib.dynamic" in settings.INSTALLED_APPS:
     urlpatterns += patterns('',
-        (r'^documents/', include('geonode.documents.urls')),
+        (r'^dynamic/', include('geonode.contrib.dynamic.urls')),
     )
 
+if 'geonode.geoserver' in settings.INSTALLED_APPS:
+    # GeoServer Helper Views
+    urlpatterns += patterns('', 
+        # Upload views
+        (r'^upload/', include('geonode.upload.urls')),
+        (r'^gs/', include('geonode.geoserver.urls')),
+    )
+
+# Set up proxy
 urlpatterns += geonode.proxy.urls.urlpatterns
 
 # Serve static files
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler403 = 'geonode.views.err403'
+
+#Featured Maps Pattens
+urlpatterns += patterns('',
+    (r'^(?P<site>[A-Za-z0-9_\-]+)/$', 'geonode.maps.views.featured_map'),
+    (r'^(?P<site>[A-Za-z0-9_\-]+)/info$', 'geonode.maps.views.featured_map_info'),
+)
